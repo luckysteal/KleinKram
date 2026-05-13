746268931285440a17f51fa82c946db2bbfe6942
luckysteal <c.klein.1999@gmx.de>
2026-05-13 21:55:36 +0200

    feat: proxy external verb conjugation API requests through local route for improved security and config management

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'german_verbs' => [
+        'url' => env('GERMAN_VERBS_API_URL', 'https://german-verbs-api.onrender.com/german-verbs-api'),
+    ],
+
 ];
